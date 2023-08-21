83787cc469ae73805251a9f0c88127a16df7cb75
aygulpardane <aygulparpucu@gmail.com>
2023-08-21 11:45:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+class Car {
+    constructor (make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    getDescription() {
+        return `This car is a ${this.year} ${this.make} ${this.model}`;
+    }
+};
+
+class ElectricCar extends Car {
+    constructor (make, model, year, range) {
+        super (make, model, year);
+        this.range = range;
+    }
+
+    getDescription() {
+        return `${super.getDescription()}. It has a range of ${this.range} miles.`
+    }
+};
+
+const tesla = new ElectricCar ("Tesla", "Model S", 2019, 300);
+console.log(tesla.getDescription());
